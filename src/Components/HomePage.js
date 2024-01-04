d4c2b3744516da3f67328c3cfe726e123c91f481
uneeth-Gaikwad <puneethgaikwadl@gmail.com>
2024-01-04 15:57:13 +0530

    added css

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -5,34 +5,34 @@ import axios from 'axios';
 import { addToCart } from '../redux/actions';
 
 const HomePage = () => {
-  const dispatch = useDispatch();
-  const products = useSelector((state) => state.products);
+    const dispatch = useDispatch();
+    const products = useSelector((state) => state.products);
 
-  useEffect(() => {
-    // Fetching products from API
-      axios.get('https://dummyjson.com/products')
-          .then((response) => {
+    useEffect(() => {
+        // Fetching products from API
+        axios.get('https://dummyjson.com/products')
+            .then((response) => {
                 dispatch({ type: 'SET_PRODUCTS', payload: response.data.products });
-              console.log(response.data.products);
-    });
-  }, [dispatch]);
+                console.log(response.data.products);
+            });
+    }, [dispatch]);
 
-  const handleAddToCart = (product) => {
-    dispatch(addToCart(product));
-  };
+    const handleAddToCart = (product) => {
+        dispatch(addToCart(product));
+    };
 
-  return (
-    <div className='card-container'>
-      {products.map((product) => (
-        <div className='card' key={product.id}>
-          <img className='img-container' src={product.images[0]} alt={product.title} />
-          <p>{product.title}</p>
-          <p>$ {product.price}</p>
-          <button className='btn' onClick={() => handleAddToCart(product)}>Add to Cart</button>
+    return (
+        <div className='card-container'>
+            {products.map((product) => (
+                <div className='card' key={product.id}>
+                    <img className='img-container' src={product.images[0]} alt={product.title} />
+                    <p>{product.title}</p>
+                    <p>$ {product.price}</p>
+                    <button className='btn' onClick={() => handleAddToCart(product)}>Add to Cart</button>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default HomePage;
--- src/Components/MyCartPage.js
+++ src/Components/MyCartPage.js
@@ -4,45 +4,48 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeFromCart, checkout } from '../redux/actions';
 
 const MyCartPage = () => {
-  const dispatch = useDispatch();
-  const myCart = useSelector((state) => state.myCart);
+    const dispatch = useDispatch();
+    const myCart = useSelector((state) => state.myCart);
 
-  const handleRemoveFromCart = (productId) => {
-    dispatch(removeFromCart(productId));
-  };
+    const handleRemoveFromCart = (productId) => {
+        dispatch(removeFromCart(productId));
+    };
 
-  const handleCheckout = () => {
-    dispatch(checkout());
-    alert('Items have been checked out!');
-  };
+    const handleCheckout = () => {
+        dispatch(checkout());
+        alert('Items have been checked out!');
+    };
 
-  return (
-    <div>
-      <h1>My Cart</h1>
-      {myCart.map((item) => (
-        <div key={item.id}>
-          <img src={item.image} alt={item.title} />
-          <p>{item.title}</p>
-          <p>${item.price}</p>
-          <button onClick={() => handleRemoveFromCart(item.id)}>
-            Remove from Cart
-          </button>
+    return (
+        <div className='my-cart'>
+            <div className='my-cart-items'>
+                <div className='card-container'>
+                    {myCart.map((item) => (
+                        <div className='card' key={item.id}>
+                            <img className='img-container' src={item.images[0]} alt={item.title} />
+                            <p>{item.title}</p>
+                            <p>${item.price}</p>
+                            <button className='btn' onClick={() => handleRemoveFromCart(item.id)}>
+                                Remove from Cart
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className='summary'>
+                <h2>Checkout List</h2>
+                <ol>
+                    {myCart.map((item) => (
+                        <li key={item.id}>
+                            <span>{item.title}</span> - <span>${item.price}</span>
+                        </li>
+                    ))}
+                </ol>
+                <p><span>Total: </span><span>${myCart.reduce((total, item) => total + item.price, 0)}</span></p>
+                <button className='btn btn2' onClick={handleCheckout}>Checkout</button>
+            </div>
         </div>
-      ))}
-      <div>
-        <h2>Checkout Summary</h2>
-        <ul>
-          {myCart.map((item) => (
-            <li key={item.id}>
-              {item.title} - ${item.price}
-            </li>
-          ))}
-        </ul>
-        <p>Total: ${myCart.reduce((total, item) => total + item.price, 0)}</p>
-        <button onClick={handleCheckout}>Checkout</button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MyCartPage;
